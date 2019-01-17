e8b95769b8b613b8fd59e82753581ce191e622cd
Kaushal Pahwani <kaushal.pahwani@talentica.com>
2019-01-17 11:43:55 +0530

    Week-2 Problem 1

---
+++ Arrays/max_difference.py
@@ -0,0 +1,17 @@
+'''
+    Raman loves Mathematics a lot. One day his maths teacher gave him an interesting problem.
+
+    He was given an array 'A' consisting of 'n' integers, he was needed to find the maximum value of the following expression:
+
+    |Ai - Aj| + |i - j|
+    where, 0<= i,j <n and Ai, Aj are the Array elements.
+'''
+
+t = input()
+while t:
+    n = input()
+    a = map(int, raw_input().split())
+    first_array = [a[i]+i for i in range(len(a))]
+    second_array = [a[i]-i for i in range(len(a))]
+    print max(max(first_array)-min(first_array), max(second_array)-min(second_array))
+    t -= 1
